848dd736737043e0c31d63a5a2919233a484d264
Vijayendra <vijayendra180806@gmail.com>
2026-07-10 13:47:24 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   // Deploying to https://Charge-Event.github.io/tgfg-website/
   base: '/tgfg-website/',
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg'],
-      manifest: false,
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      },
-    }),
-  ],
+  plugins: [react()],
 })
